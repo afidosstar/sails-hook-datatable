9d3ccb656108f094e44d9997d0f78cc46b518c72
afidoss <afidosstar@gmail.com>
2017-04-01 00:18:32 +0100

    Edit of query data in data base

--- api/services/DatatableService.js
+++ api/services/DatatableService.js
@@ -12,10 +12,13 @@ module.exports.getData = function (model, options) {
     })
   }
 
-  var ATTR = Object.keys(MODEL._attributes)
-
   //possible column options as default
   var _columns = [{ data: '', name: '', searchable: false, orderable: false, search: { value: '' } }]
+    //default order
+    //Column to which ordering should be applied.
+    // This is an index reference to the columns array of information that is also submitted to the server.
+
+  var order = [{ column: 0, dir: 'asc' }]
 
   //possible options data as default
   var _options = {
@@ -24,11 +27,12 @@ module.exports.getData = function (model, options) {
     start: 0,
     length: 10,
     search: { value: '', regex: false },
-    order: [{ column: 0, dir: 'asc' }]
+    order: order
   }
 
   //merge both Object, options and _options into _options
   Object.assign(_options, options)
+
 
   //response format
   var _response = {
@@ -40,52 +44,88 @@ module.exports.getData = function (model, options) {
     data: []
   }
 
-  //build where criteria
-  var where = [], whereQuery = { or: [] }, select = []
 
-  if (_options.columns.length) {
-    if (_options.columns[0].data == 0) {//type array
-      /**
-       * sails never responds with an array of literals or primitives like [["boy","foo"], ["girl","bar"]]
-       * do set your column.data attribute from your datatable config
-       */
-    } else {//type Object
-      _options.columns.forEach((column, index) => {
-        if (column.searchable) {
-          if (column.data.indexOf('.') > -1) {//accesing object attribute for value
-            var col = column.data.substr(0, column.data.indexOf('.'))
-            var filter = {}
-            filter[col] = {
-              'contains': _options.search.value
-            }
-            select.push(col)
-            where.push(filter)
-          } else {
-            var filter = {}
-            filter[column.data] = {
-              'contains': _options.search.value
-            }
-            select.push(column.data)
-            where.push(filter)
-          }
+
+/*
+   //build where criteria
+   var where = [], whereQuery = { or: [] }, select = [] , order = []
+
+   if (_options.columns.length) {
+   if (_options.columns[0].data == 0) {//type array
+   /!**
+   * sails never responds with an array of literals or primitives like [["boy","foo"], ["girl","bar"]]
+   * do set your column.data attribute from your datatable config
+   *!/
+   } else {//type Object
+   _options.columns.forEach((column, index) => {
+   if (column.searchable) {
+   if (column.data.indexOf('.') > -1) {//accesing object attribute for value
+   var col = column.data.substr(0, column.data.indexOf('.'))
+   var filter = {}
+   filter[col] = {
+   'contains': _options.search.value
+   }
+   select.push(col)
+   where.push(filter)
+   } else {
+   var filter = {}
+   filter[column.data] = {
+   'contains': _options.search.value
+   }
+   select.push(column.data)
+   where.push(filter)
+   }
+   }
+   })
+   }
+   whereQuery = { or : where }
+   } else {
+   whereQuery = {}
+   }
+*/
+
+  var where = [], whereQuery = { or: []}, select = [] , order = []
+
+  _options.columns.forEach(function (column, index) {
+    if (column.searchable) {
+      if (column.data) {
+        var property = column.data.split('.').shift()
+
+        if (!property) return null
+
+        select.push(property)
+
+        if ( column.search.value) {
+          whereQuery[property] = column.search.regex ? { contains : column.search.value } :  column.search.value
         }
-      })
+
+        if (_options.search.value){
+          var or = {}
+          or[property] = _options.search.regex ? { contains : _options.search.value } :  _options.search.value
+          whereQuery.or.push(or)
+        }
+      }
     }
-    whereQuery = { or : where }
-  } else {
-    whereQuery = {}
-  }
+  })
+  if(whereQuery.or.length < 1) delete whereQuery.or
+  if(select.length < 1) select: "*"
 
 
+  _options.order.forEach(function (_order, index) {
+    if (_options.columns[_order.column].orderable) {
+      order.push(_options.columns[_order.column].data+ " " +_order.dir.toUpperCase())
+    }
+  })
 
-  //find the databased on the query and total items in the database data[0] and data[1] repectively
-  return Promise.all([MODEL.find({
+  var query = {
     where: whereQuery,
     select: select,
     skip: +_options.start,
     limit: +_options.length,
-    sort: _options.columns[+_options.order[0].column].data + ' ' + _options.order[0].dir.toUpperCase()
-  }), MODEL.count()]).then(data => {
+    sort: order
+  }
+  //find the databased on the query and total items in the database data[0] and data[1] repectively
+  return Promise.all([MODEL.find(query), MODEL.count()]).then(data => {
     _response.recordsTotal = data[1]//no of data stored
     _response.recordsFiltered = data[0].length//no of data after applying filter
     _response.iTotalRecords = data[1]//no of data stored (legacy)
